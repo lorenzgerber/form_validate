927f9a55fcb6472bb44dee91b428a22f75cd2ae0
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-11-18 11:23:13 +0100

    working on validation

--- konferens/scripts/validate.js
+++ konferens/scripts/validate.js
@@ -1,5 +1,11 @@
 window.onload = attachValidation;
 
+var validObjects = Array();
+validObjects[0] = {fieldName:'field_firstname', fieldText:'First name', missing:true};
+validObjects[1] = {fieldName:'field_lastname', fieldText:'Last name', missing:true};
+validObjects[2] = {fieldName:'field_organisation', fieldText:'Organisation', missing:true};
+
+
 function attachValidation() {
   var form = document.getElementById('registration_form');
   if (form.attachEvent) {
@@ -10,15 +16,32 @@ function attachValidation() {
 }
 
 function validateForm(event) {
-  var firstName = document.forms['registration_form']['field_firstname'].value;
-  var lastName = document.forms['registration_form']['field_lastname'].value;
-  var organisation = document.forms['registration_form']['field_organisation'].value;
-  if(firstName.length != 0){
-    alert(firstName);
-    return true;
-  } else {
-    alert('First Name is missing');
-    event.preventDefault();
-    return false;
+  var validationOk = true;
+  var missingInput ='';
+
+  for(var i = 0; i < validObjects.length; i++){
+    if(!entryExists(validObjects[i])){
+      event.preventDefault();
+      validationOk = false;
+      if(i == validObjects.length -1){
+        missingInput = missingInput + validObjects[i].fieldText + '.';
+      } else {
+        missingInput = missingInput + validObjects[i].fieldText + ', ';
+      }
+    }
   }
+  if(!validationOk){
+    alert("Please enter the following missing values\n" + missingInput);
+
+  }
+}
+
+function entryExists(validObj){
+
+  var field = document.forms['registration_form'][validObj.fieldName].value;
+  if(field.length != 0){
+    validObj.missing = false;
+    return true;
+  }
+  return false;
 }
