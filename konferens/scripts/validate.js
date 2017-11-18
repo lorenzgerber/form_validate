f84f7edb4848df43e1354af47baba995b29071b0
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-11-18 15:10:13 +0100

    email check

--- konferens/scripts/validate.js
+++ konferens/scripts/validate.js
@@ -1,10 +1,26 @@
 window.onload = attachValidation;
 
 var validObjects = Array();
-validObjects[0] = {fieldName:'field_firstname', fieldText:'First name', missing:true};
-validObjects[1] = {fieldName:'field_lastname', fieldText:'Last name', missing:true};
-validObjects[2] = {fieldName:'field_organisation', fieldText:'Organisation', missing:true};
 
+validObjects[0] = {
+  fieldName:'field_firstname',
+  fieldText:'Förnamn',
+  missing:true};
+
+validObjects[1] = {
+  fieldName:'field_lastname',
+  fieldText:'Efternamn',
+  missing:true};
+
+validObjects[2] = {
+  fieldName:'field_organisation',
+  fieldText:'Organisation',
+  missing:true};
+
+validObjects[3] = {
+  fieldName:'field_email',
+  fieltText:'E-mail',
+  missing:true};
 
 function attachValidation() {
   var form = document.getElementById('registration_form');
@@ -23,7 +39,7 @@ function validateForm(event) {
     if(!entryExists(validObjects[i])){
       event.preventDefault();
       validationOk = false;
-      if(i == validObjects.length -1){
+      if(i == validObjects.length-1){
         missingInput = missingInput + validObjects[i].fieldText + '.';
       } else {
         missingInput = missingInput + validObjects[i].fieldText + ', ';
@@ -31,7 +47,21 @@ function validateForm(event) {
     }
   }
   if(!validationOk){
-    alert("Please enter the following missing values\n" + missingInput);
+    missingInput = 'Du måste fylla i:\n' + missingInput;
+  }
+
+  if(entryExists(validObjects[3])){
+    email = document.forms['registration_form'][validObjects[3].fieldName].value;
+    if(!checkEmail(email)){
+      event.preventDefault;
+      validationOk = false;
+      missingInput = missingInput + '\n\n' + 'Fel i email adress formatet.'
+    }
+  }
+
+
+  if(!validationOk){
+    alert(missingInput);
 
   }
 }
@@ -44,4 +74,9 @@ function entryExists(validObj){
     return true;
   }
   return false;
+}
+
+function checkEmail(email){
+  var pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+  return pattern.test(email);
 }
